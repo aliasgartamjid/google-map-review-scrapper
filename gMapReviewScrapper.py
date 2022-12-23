b1826dc06f589c8facdb3e99c19fa8e3f956e02a
Ali Asgar Tamjid <102575178+already101@users.noreply.github.com>
2022-12-23 17:36:32 +0600

    Add files via upload

--- gMapReviewScrapper.py
+++ gMapReviewScrapper.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
-
-
-website='https://www.google.com/maps/place/Salauddin+Specialized+Hospital+Ltd./@23.7169386,90.4178704,17z/data=!4m7!3m6!1s0x3755b9c74641e61f:0x51a0480487a88dc1!8m2!3d23.7169337!4d90.4200591!9m1!1b1'
+website='https://www.google.com/maps/place/Evercare+Hospital+Dhaka/@23.8106745,90.4301011,18.85z/data=!4m7!3m6!1s0x3755c64aaa9e9039:0x226faca86a7e028d!8m2!3d23.8102668!4d90.4313219!9m1!1b1?hl=en-BD'
 driver = webdriver.Chrome()
 driver.get(website)
 
